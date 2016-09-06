ccb35c8c610c6f3ccced59e95d695a6632c77f2f
Felipe Soares <soaresfelipef@gmail.com>
2016-09-06 00:52:17 -0300

    Application class created

---
+++ app/src/main/java/com/soaresfelipef/top10downloader/Application.java
@@ -0,0 +1,35 @@
+package com.soaresfelipef.top10downloader;
+
+/**
+ * Created by felipesoares on 06/09/16.
+ */
+public class Application {
+
+    private String name;
+    private String artist;
+    private String releaseDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
