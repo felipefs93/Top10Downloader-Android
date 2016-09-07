34b2958fb933c7477d2ca218896060f17e0ceb2a
Felipe Soares <soaresfelipef@gmail.com>
2016-09-07 13:37:23 -0300

    Logs from applications

--- app/src/main/java/com/soaresfelipef/top10downloader/ParseApplications.java
+++ app/src/main/java/com/soaresfelipef/top10downloader/ParseApplications.java
@@ -76,6 +76,13 @@ public class ParseApplications {
             e.printStackTrace();
         }
 
+        for(Application app : applications){
+            Log.d("ParseApplications","******************");
+            Log.d("ParseApplications","Name: " + app.getName());
+            Log.d("ParseApplications","Artist: " + app.getArtist());
+            Log.d("ParseApplications","Release Date" + app.getReleaseDate());
+        }
+
         return true;
     }
 }
